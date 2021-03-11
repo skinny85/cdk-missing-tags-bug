c7ca1839f31a403cdaae6449548228442e26b612
Adam Ruka <adamruka@amazon.com>
2021-03-10 17:05:11 -0800

    Update unit test.

--- test/missing-tags.test.ts
+++ test/missing-tags.test.ts
@@ -1,4 +1,4 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { expect as expectCDK, haveResourceLike } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as MissingTags from '../lib/missing-tags-stack';
 
@@ -7,7 +7,5 @@ test('Empty Stack', () => {
     // WHEN
     const stack = new MissingTags.MissingTagsStack(app, 'MyTestStack');
     // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+    expectCDK(stack).to(haveResourceLike('AWS::ServiceCatalog::CloudFormationProduct'));
 });
