bf5733e622bebca117f46e9210c4cb53b24bd7ba
Adam Ruka <adamruka@amazon.com>
2021-03-10 16:35:08 -0800

    Add CfnCloudFormationProduct resource, and tag it on the Stack level.

--- bin/missing-tags.ts
+++ bin/missing-tags.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
+
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { MissingTagsStack } from '../lib/missing-tags-stack';
 
 const app = new cdk.App();
-new MissingTagsStack(app, 'MissingTagsStack');
+const stack = new MissingTagsStack(app, 'MissingTagsStack');
+cdk.Tags.of(stack).add('Product', 'CICD');
--- lib/missing-tags-stack.ts
+++ lib/missing-tags-stack.ts
@@ -1,9 +1,23 @@
 import * as cdk from '@aws-cdk/core';
+import * as sc from '@aws-cdk/aws-servicecatalog';
 
 export class MissingTagsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Repo Baseline Product
+    const scProduct = new sc.CfnCloudFormationProduct(this, 'RepoBaselineProduct', {
+      name: 'test-product',
+      owner: 'Me',
+      description: 'My Product.',
+      provisioningArtifactParameters: [
+        {
+          name: 'v0.1.0',
+          info: {
+            LoadTemplateFromURL: 'https://whatever.s3.amazonaws.com/whatever',
+          }
+        },
+      ]
+    });
   }
 }
